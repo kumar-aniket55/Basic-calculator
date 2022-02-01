9dc957b4e69d83ebb23b52c19c7425b0846c4948
kumar-aniket55 <79923308+kumar-aniket55@users.noreply.github.com>
2022-02-01 20:22:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+let result_string="";
+let num;
+function input_value(x)
+{
+    if(document.getElementById("input").value=="INVALID")
+    {
+        document.getElementById("input").value="";
+        result_string="";
+    }
+          
+    if(document.getElementById("input").value=='ENTER')
+    {
+        document.getElementById("input").value="";
+    }
+    if(x=="!")
+    {
+         num=parseInt(result_string);
+    let result=num;
+    if(result==0)
+    {
+        result=1;
+    }
+    else{
+        for(let i=num-1;i>1;i--)
+    {
+        result*=i;
+    }
+
+    }
+    
+    result_string=result;
+    document.getElementById("input").value+=x;
+    }
+    else if(x=="%")
+    {
+        document.getElementById("input").value+=x;
+    result_string+="/100";
+    }
+    else if(x=="e")
+    {
+        document.getElementById("input").value+=x;
+        result_string+="2.71828182846";
+
+    }
+    
+   else
+   {
+    document.getElementById("input").value+=x;
+    result_string+=x;
+   }
+
+}
+function pi(x)
+{
+    document.getElementById("input").value+=x;
+    result_string+="3.14159265359";
+
+}
+function clear_input()
+{
+    document.getElementById("input").value='ENTER';
+    result_string="";
+
+}
+function expo()
+{
+    result_string+="**";
+    document.getElementById("input").value+="^";
+    
+    // result_string+="**";
+
+}
+function output_result()
+{
+     
+      try {
+        document.getElementById("input").value=eval(result_string);
+      
+    } catch (e) {
+        if (e instanceof SyntaxError) {
+            document.getElementById("input").value="INVALID"
+            console.log(e);
+        }
+    }
+
+   
+}
+function factorial()
+{
+    let num=parseInt(result_string);
+    let result=num;
+    for(let i=num-1;i>1;i--)
+    {
+        result*=1;
+    }
+    result_string=result;
+}
